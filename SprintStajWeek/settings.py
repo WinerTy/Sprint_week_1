9c08cc312b4b61b2abfeb91df296546595d93e6e
WinerTy <luchiy.v.mire@mail.ru>
2024-01-17 14:43:00 +0700

    add APP for SubmitData

--- SprintStajWeek/settings.py
+++ SprintStajWeek/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'database.apps.DatabaseConfig',
+    'API.apps.ApiConfig',
+
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
