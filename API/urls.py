2f629089165c89cc5b22bd39c3e19ccbb5d0704d
WinerTy <luchiy.v.mire@mail.ru>
2024-02-12 03:02:14 +0700

    uppdate

--- API/urls.py
+++ API/urls.py
@@ -13,16 +13,6 @@ router.register(r"images", views.ImagesViewset)
 router.register(r"submitData", views.PerevalsViewset, basename="submitdata")
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Документация для REST API(DRF)",
-        default_version="v1.0.0",
-        description="Инструкция пользовнаия API",
-    ),
-    public=True,
-)
-
-
 urlpatterns = [
     path("", include(router.urls)),
 ]
